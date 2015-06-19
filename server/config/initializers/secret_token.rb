ed94265c1013b24b5841ef6cd787b858284d41ff
Kelsey Pedersen <kelseypedersen@gmail.com>
2015-06-19 10:49:16 -0700

    Add rails api server postgres database

---
+++ server/config/initializers/secret_token.rb
@@ -0,0 +1,19 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+
+# Although this is not needed for an api-only application, rails4 
+# requires secret_key_base or secret_token to be defined, otherwise an 
+# error is raised.
+# Using secret_token for rails3 compatibility. Change to secret_key_base
+# to avoid deprecation warning.
+# Can be safely removed in a rails3 api-only application.
+Server::Application.config.secret_token = '1d0797527bf68d603c23053d19100a62bae3ee8238ee1093f9ec0c027fee1ccce1dce04411621dabca45aaa82e734d63c6548e6851c9c4c33866579672d952fc'
