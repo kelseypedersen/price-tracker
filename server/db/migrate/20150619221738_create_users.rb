be9cd78f251a97af986052528316380dce950062
Kelsey Pedersen <kelseypedersen@gmail.com>
2015-06-19 16:38:22 -0700

    fix migration for users to have many wishlists, incorp name email and password

--- server/db/migrate/20150619221738_create_users.rb
+++ server/db/migrate/20150619221738_create_users.rb
@@ -2,6 +2,10 @@ class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
 
+      t.string :name
+      t.string :password
+      t.string :email
+
       t.timestamps null: false
     end
   end
