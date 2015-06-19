9eb53a9c9e0ea74c62a810a7c276f531dd25e4bd
Kelsey Pedersen <kelseypedersen@gmail.com>
2015-06-19 16:40:49 -0700

    create products table

--- server/db/migrate/20150619221748_create_products.rb
+++ server/db/migrate/20150619221748_create_products.rb
@@ -2,6 +2,8 @@ class CreateProducts < ActiveRecord::Migration
   def change
     create_table :products do |t|
 
+      t.integer :shopstyle_id, :limit => 8
+
       t.timestamps null: false
     end
   end
