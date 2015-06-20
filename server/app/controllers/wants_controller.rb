c9e3bdccf8eb8ef22c72f80b803ac00c88a1e006
Kelsey Pedersen <kelseypedersen@gmail.com>
2015-06-20 12:46:23 -0700

    Add routes without the comments at the bottom of the file

--- server/app/controllers/wants_controller.rb
+++ server/app/controllers/wants_controller.rb
@@ -43,37 +43,3 @@ class WantsController < ApplicationController
   end
 
 end
-
-
-
-# before_action :user_find, only: [:show, :edit, :destroy, :update]
-
-#   def new
-#     @user = User.new
-#   end
-
-#   def show
-#     render json: {user: @user}
-#   end
-
-#   def create
-#     @user = User.new(id: params[:id], name: params[:name], password: params[:password], email: params[:email])
-#     if @user.save
-#       render json: { user: @user }, status: :created
-#     else
-#       render json: @user.errors.full_messages, status: :unprocessable_entity
-#     end
-#   end
-
-#   def edit
-#     render json: {user: @user}
-#   end
-
-
-
-#   def update
-#     @user.update(name: params[:name], password: params[:password], email: params[:email])
-#   end
-
-
-
