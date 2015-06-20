068e2313f271c8dac3f8fd219b48adb475ee8557
Kelsey Pedersen <kelseypedersen@gmail.com>
2015-06-20 12:29:27 -0700

    Add json crud routes to product controller

--- server/app/controllers/products_controller.rb
+++ server/app/controllers/products_controller.rb
@@ -1,15 +1,38 @@
 class ProductsController < ApplicationController
 
+  before_action :product_find
+
   def index
+    @users = Product.all
   end
 
   def show
+    render json: {product: @product}
+  end
+
+  def new
+    @product = Product.new
   end
 
   def create
+    @product = Product.new(id: params[:id], shopstyle_id: params[:shopstyle_id])
+    if @product.save
+      render json: { product: @product }, status: :created
+    else
+      render json: @product.errors.full_messages, status: :unprocessable_entity
+    end
   end
 
   def destroy
+    @product.destroy
+  end
+
+  private
+
+  def product_find
+    @product = Product.find(params[:id])
   end
 
 end
+
+
