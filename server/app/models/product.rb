7e4890f70a2dff262cf1dee09472237c056c363f
Mary Wallace <wallaceemee@gmail.com>
2015-06-21 17:53:23 -0700

    clean up the code

--- server/app/models/product.rb
+++ server/app/models/product.rb
@@ -2,10 +2,4 @@ class Product < ActiveRecord::Base
 
   belongs_to :want
 
-  # def self.search(search)
-  #   if search
-  #     where(:all, :conditions => ["name LIKE ?", "%#{search}%"])
-  #   end
-  # end
-
 end
