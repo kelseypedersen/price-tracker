be9d9fb0369149e09a0ed3fa18906a7be27ed14d
Danielle Rigsby <drigsby@hotmail.com>
2015-06-22 19:44:29 -0700

    correct indentation in migration file

--- db/migrate/201506221935_change_wants.rb
+++ db/migrate/201506221935_change_wants.rb
@@ -1,5 +1,5 @@
 class ChangeWants < ActiveRecord::Migration
   def change
-        add_column :wants, :notified?, :boolean, :default => false
+    add_column :wants, :notified?, :boolean, :default => false
   end
 end
