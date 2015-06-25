73b351b6d87b2900439c030f907cb4d77a88acb6
Danielle Rigsby <drigsby@hotmail.com>
2015-06-25 15:06:03 -0700

    made a small change to the seed file to eliminate a conflict

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,7 +3,7 @@ require 'faker'
 products = [480234994, 481587986]
 
 3.times do
-  user = User.create(name: Faker::Name.first_name, password: Faker::Internet.password(6), email: Faker::Internet.email, phone_number: "4157704113")
+  user = User.create(name: Faker::Name.first_name, password: Faker::Internet.password(6), email: Faker::Internet.email)
   product = Product.create(shopstyle_id: products[rand(0..1)])
 end
 
